87adb781a7eea620d9952218e3af169088b82735
NicolasMaxime <maxime1.nicolas@gmail.com>
2021-01-27 23:08:16 +0100

    beautify promotion & participation

--- src/app/participation/participation.component.ts
+++ src/app/participation/participation.component.ts
@@ -5,6 +5,8 @@ import {Bid} from '../shared/interfaces/bid';
 import {BidService} from '../shared/services/bid.service';
 import {User} from '../shared/interfaces/user';
 import {AuthenticationService} from '../shared/services/authentication.service';
+import {ParticipationService} from '../shared/services/participation.service';
+import {Participation} from '../shared/interfaces/participation';
 
 @Component({
   selector: 'app-participation',
@@ -14,17 +16,23 @@ import {AuthenticationService} from '../shared/services/authentication.service';
 export class ParticipationComponent implements OnInit {
 
   private _bids: Bid[];
-  private _displayedColumns: string[] = ['nom', 'description', 'info'];
+  private _displayedColumns: string[] = ['nom', 'description', 'endDate', 'bestPrice', 'winner', 'info'];
   private _currentUser: User;
 
-  constructor(private readonly _bidService: BidService, private readonly _authenticationService: AuthenticationService) {
+  constructor(private readonly _bidService: BidService, private readonly _authenticationService: AuthenticationService,
+              private readonly _participationService: ParticipationService) {
     this._authenticationService.currentUser.subscribe(x => this._currentUser = x);
   }
 
   ngOnInit(): void {
     this._bidService.fetchParticipationUser(this._currentUser.id).subscribe(
-      (_: Bid[]) => this._bids = _,
+      (_: Bid[]) => {this._bids = _; this._addBestBidder(); return; },
     );
+  }
+
+  isExpired(date: Date): boolean {
+    let today = new Date();
+    return (new Date(date)).getTime() < today.getTime();
   }
 
   get bids(): Bid[]{
@@ -33,5 +41,21 @@ export class ParticipationComponent implements OnInit {
 
   get displayedColumns(): string[]{
     return this._displayedColumns;
+  }
+
+  private _addBestBidder() {
+    for(let i = 0; i != this._bids.length; i++)
+      this._participationService.fetchBest(this._bids[i].id)
+        .subscribe((tmp: Participation) => {
+          this._bids[i].won = false;
+          this._bids[i].best = tmp.price;
+            if (tmp.idUser == this._currentUser.id)
+              this._bids[i].won = true;
+          }
+        );
+  }
+
+  logged(): boolean {
+    return this._authenticationService.logged();
   }
 }
--- src/app/promotions/promotions.component.ts
+++ src/app/promotions/promotions.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PromotionService} from '../shared/services/promotion.service';
 import {Promotion} from '../shared/interfaces/promotion';
+import {Participation} from '../shared/interfaces/participation';
+import {ParticipationService} from '../shared/services/participation.service';
 
 @Component({
   selector: 'app-promotions',
@@ -10,13 +12,13 @@ import {Promotion} from '../shared/interfaces/promotion';
 export class PromotionsComponent implements OnInit {
 
   private _promotions: Promotion[];
-  private _displayedColumns: string[] = ['nom', 'reduction', 'description', 'info'];
-  constructor(private readonly _promotionService: PromotionService) { }
+  private _displayedColumns: string[] = ['nom', 'reduction', 'description', 'endDate', 'bestPrice', 'info'];
+  constructor(private readonly _promotionService: PromotionService,
+              private readonly _participationService: ParticipationService) { }
 
   ngOnInit(): void {
     this._promotionService.fetch().subscribe(
-      (_: Promotion[]) => this._promotions = _,
-    );
+      (_: Promotion[]) => {this._promotions = _; this._addBestBidder(); return; });
   }
 
   get promotions(): Promotion[]{
@@ -25,5 +27,24 @@ export class PromotionsComponent implements OnInit {
 
   get displayedColumns(): string[]{
     return this._displayedColumns;
+  }
+
+  private _addBestBidder() {
+    for(let i = 0; i != this._promotions.length; i++) {
+      this._participationService.fetchBest(this._promotions[i].article.id)
+        .subscribe((tmp: Participation | null) => {
+            console.log("here");
+            if (tmp && tmp.price)
+              this._promotions[i].best = tmp.price;
+            else
+              this._promotions[i].best = this._promotions[i].article.startPrice;
+          }
+        );
+    }
+  }
+
+  isExpired(date: Date): boolean {
+    let today = new Date();
+    return (new Date(date)).getTime() < today.getTime();
   }
 }
